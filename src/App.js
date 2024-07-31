f6437ff22cf4104a8a44998d52df9bbbbb8571c5
Sandhana-Krishnan-S <sandhanakrishnansakthivel@gmail.com>
2024-08-01 00:32:15 +0530

    sandu fix - Complete

--- src/App.js
+++ src/App.js
@@ -66,4 +66,15 @@ function App() {
   );
 }
 
+// export default App;
+
+
+// function App() {
+//   return (
+//     <div>
+//       <h1>Hello World</h1>
+//     </div>
+//   );
+// }
+
 export default App;
