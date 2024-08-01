a1ed0231cb32a9e7092be975dd45f8bdfdca5214
Sandhana-Krishnan-S <sandhanakrishnansakthivel@gmail.com>
2024-08-01 15:20:36 +0530

    version and babble - fix

--- src/index.js
+++ src/index.js
@@ -1,10 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'font-awesome/css/font-awesome.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min';
-import $ from 'jquery';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
